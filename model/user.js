bb493f2427a0563bd48ba57298c6089d00cdad37
Emma VANDECASTEELE <etu40268@henallux.be>
2020-11-03 18:45:07 +0100

    add password crypto

--- model/user.js
+++ model/user.js
@@ -1,7 +1,13 @@
 const UserDAO = require('../dao/userDAO');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 module.exports.createUser = async (client, user) => {
 
+
+    // crypt password
+    user.password = bcrypt.hashSync(user.password, saltRounds);
 
     //TODO #4 business validation
     
