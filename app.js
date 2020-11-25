c03c2e48a8eb5cc283008205d2edfc527a820f22
emmavdc <etu40268@henallux.be>
2020-11-25 21:01:14 +0100

    add cors and addadmin

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const Router = require('./route');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(Router);
 
 app.listen(process.env.PORT, () => {
