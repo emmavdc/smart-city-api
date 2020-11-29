b9d3294a06ecb95282d927f5875641590bcdd5ab
emmavdc <etu40268@henallux.be>
2020-11-29 20:35:24 +0100

    login user unknow in database fix

--- model/user.js
+++ model/user.js
@@ -1,43 +1,47 @@
-const UserDAO = require('../dao/userDAO');
-const bcrypt = require('bcrypt');
+const UserDAO = require("../dao/userDAO");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 module.exports.createUser = async (client, user) => {
+  // crypt password
+  user.password = bcrypt.hashSync(user.password, saltRounds);
 
+  //TODO #4 business validation
 
-    // crypt password
-    user.password = bcrypt.hashSync(user.password, saltRounds);
-
-    //TODO #4 business validation
-
-    const userId = await UserDAO.insertUser(client, user);
-    return jwt.sign({ email: user.email, userId : userId , exp: Math.floor(Date.now() / 1000) + (60 * 60)}, process.env.SECRET);
-
-}
-
+  const userId = await UserDAO.insertUser(client, user);
+  return jwt.sign(
+    {
+      email: user.email,
+      userId: userId,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60,
+    },
+    process.env.SECRET
+  );
+};
 
 module.exports.loginUser = async (client, user) => {
+  const { rows: users } = await UserDAO.selectUser(client, user);
 
-    const {rows: users} = await UserDAO.selectUser(client, user);
-
-    if(bcrypt.compareSync(user.password, users[0].password)) {
-        return jwt.sign({ email: user.email, userId : user.userId , exp: Math.floor(Date.now() / 1000) + (60 * 60)}, process.env.SECRET);
+  if (users[0]) {
+    if (bcrypt.compareSync(user.password, users[0].password)) {
+      return jwt.sign(
+        {
+          email: user.email,
+          userId: user.userId,
+          exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        },
+        process.env.SECRET
+      );
     }
-    else {
-        return;
-    }
-   
-}
+  }
+  return;
+};
 
-module.exports.updateUser = async(client, user, userId) =>{
+module.exports.updateUser = async (client, user, userId) => {
+  user.password = bcrypt.hashSync(user.password, saltRounds);
 
-    user.password = bcrypt.hashSync(user.password, saltRounds);
+  //todo #8 business check (email exists + return errors)
 
-    //todo #8 business check (email exists + return errors)
-
-    return await UserDAO.updateUser(client, user, userId);
-}
-
-
+  return await UserDAO.updateUser(client, user, userId);
+};
